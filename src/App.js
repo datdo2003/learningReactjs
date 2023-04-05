e97276ec8ac80e46a3933b472fa9380b1d560cd0
Dat Do <dat.do@axonactive.com>
2023-04-05 17:23:40 +0700

    Using hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Heading from "./components/Heading";
+import Btn from "./components/Btn";
+import Toggler from "./components/Toggler";
+import Header from "./components/Header";
+import InputComponent from "./components/InputComponent";
+const data = {
+      title: "99% sale off!",
+      callToAction: "All items must go"
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return <Header />
+  // try {
+  //   (4).toUpperCase();
+  // } catch (e) {
+  //   console.log("Oops, something went wrong", e);
+  // }
+  // return <Heading />
+  // return <Btn/>
+  // return <Toggler />
+  // return <Header title={data.title} callToAction={data.callToAction}/>
+  return <InputComponent />
 }
+
+// function Header() {
+//   return <h1>Hello world!!!</h1>;
+// }
 
 export default App;
